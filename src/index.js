86bbe6603e987067e5633271d72ee9512d91ab15
duy1111 <maixuanduy0605@gmail.com>
2023-02-12 16:10:52 +0700

    pick Access-Control-Allow-Origin #2

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 //app.use(cors());
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
