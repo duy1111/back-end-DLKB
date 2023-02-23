5f7dcbf45340ffca42a7224403dc0f785dbc87ba
duy1111 <maixuanduy0605@gmail.com>
2023-02-23 21:46:07 +0700

    update messenger plugin#6

--- src/services/chatboxServices.js
+++ src/services/chatboxServices.js
@@ -21,7 +21,7 @@ let callSendApi = (sender_psid,response) => {
         },
         (err, res, body) => {
             if (!err) {
-                console.log('message sent!');
+                console.log(body);
             } else {
                 console.error('Unable to send message:' + err);
             }
@@ -79,43 +79,33 @@ let handleGetStarted = (sender_psid) => {
 };
 let sendGetStartedTemplate = () =>{
     let response = {
-        attachment: {
-            type: 'template',
-            payload: {
-                template_type: 'generic',
-                elements: [
-                    {
-                        title: 'Chào mừng bạn đến với Doctor Booking',
-                        subtitle: 'Dưới đây là các lựa chọn của Doctor Booking',
-                        image_url: IMAGE_GET_STARTED,
-                        buttons: [
-                            {
-                                type: 'postback',
-                                title: 'XEM PHÒNG KHÁM',
-                                payload: 'yes',
-                            },
-                            {
-                                type: 'postback',
-                                title: 'CHỌN BÁC SĨ',
-                                payload: 'no',
-                            },
-                            {
-                                type: 'postback',
-                                title: 'ĐẶT LỊCH KHÁM',
-                                payload: 'no',
-                            },
-                            {
-                                type: 'postback',
-                                title: 'HƯỚNG DẪN SỬ DỤNG BOT',
-                                payload: 'no',
-                            },
-                        ],
-                    },
-                ],
-            },
-        },
+        "persistent_menu": [
+        {
+            "locale": "default",
+            "composer_input_disabled": false,
+            "call_to_actions": [
+                {
+                    "type": "postback",
+                    "title": "Talk to an agent",
+                    "payload": "CARE_HELP"
+                },
+                {
+                    "type": "postback",
+                    "title": "Outfit suggestions",
+                    "payload": "CURATION"
+                },
+                {
+                    "type": "web_url",
+                    "title": "Shop now",
+                    "url": "https://www.originalcoastclothing.com/",
+                    "webview_height_ratio": "full"
+                }
+            ]
+        }
+    ]
+
     };
-    return response
+    return response;
 }
 module.exports = {
     handleGetStarted: handleGetStarted,
